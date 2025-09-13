9d853d78070b03ba6be56b397385a62eeeca8501
Juan José Torres Becerra <166084568+JuanJTorresB@users.noreply.github.com>
2025-09-13 09:03:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ cell_size = 30
 number_of_cells = 25
 
 class Food:
-   def __init__(self):
-     self.position = self.generate_random_pos()
+   def __init__(self, snake_body):
+     self.position = self.generate_random_pos(snake_body)
     
    def draw(self):
      food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
      screen.blit(food_surface, food_rect)
-     
-   def generate_random_pos(self):
+   
+   def generate_random_cell(self):
      x = random.randint(0, number_of_cells - 1)
      y = random.randint(0, number_of_cells - 1)
-     position = Vector2(x, y)
+     return Vector2(x, y)
+     
+   def generate_random_pos(self, snake_body):
+     
+     position = self.generate_random_cell()
+     
+     while position in snake_body:
+       position = self.generate_random_cell()
+     
      return position
      
 class Snake:
